9e8e5210d9cec0286410b83f316ae0479eb9c75f
Lemniscate <umair.ops@gmail.com>
2015-07-10 13:44:42 -0400

    updated subtree script

--- import_repository_as_git_subtree.sh
+++ import_repository_as_git_subtree.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Author - Lemniscate - 09-July-2015
+#Author - Lemniscate Snickets - 09-July-2015
 
 #Script to import a repo as a git subtree
 
@@ -16,6 +16,29 @@ echo " |-If not then you should be"
 else
 #presumably in the top level git dir
 #ok now do the thing
+
+#Now to check if there are any commits pending 
+gitstatus="$(git status)"
+branchupdatestring='Your branch is up-to-date with'
+commitstring='nothing to commit, working directory clean'
+
+#check if git status contains the string 
+
+if echo "$gitstatus" | grep -q "$commitstring"; then
+	echo "|-It looks like everything is uptodate before subtree import - Continuing";
+else
+	echo "|-It looks like there are some commits pending"
+	echo "|-It is advisable to commit them"
+	echo "|-Do you want to exit to commit (n) or try to continue (y) ?"
+	commitcheck='$1'
+	read commitcheck
+
+	if [ "$commitcheck" != 'n' ] || [ "$commitcheck" != 'no' ]; then
+		exit 0 # exit script they want to commit changes 
+	fi
+
+fi
+
 
 echo "|- Please enter the dir where you would like to make the repo as a subtree"
 echo " |- Path to dir  full/reletive - if dir does not exist it will create it"
@@ -49,12 +72,6 @@ echo "***********************"
 echo "All done"
 echo "For push pull to subtree use the following remotes as from the top level dir"
 echo "git pull -s subtree remotename branchname"
-git remote -v 
+git remote -v
 
 fi
-
-
-
-
-
-
